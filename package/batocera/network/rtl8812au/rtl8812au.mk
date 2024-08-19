a539c9f596163c1d6943642853894ae84eed97e2
lzretrogames <lzretrogames@gmail.com>
2024-08-19 08:42:35 -0500

    RTL8812-DEU ERRO

--- package/batocera/network/rtl8812au/rtl8812au.mk
+++ package/batocera/network/rtl8812au/rtl8812au.mk
@@ -3,9 +3,9 @@
 # rtl8812au
 #
 ################################################################################
-# Version: Commits on Jul 28, 2023
-RTL8812AU_VERSION = b5f4e6e894eca8fea38661e2fc22a2570e0274ad
-RTL8812AU_SITE = $(call github,morrownr,8812au-20210629,$(RTL8812AU_VERSION))
+# Version: Commits on May 28, 2024
+RTL8812AU_VERSION = b350eb84be9808a6748c81711c187e205889e968
+RTL8812AU_SITE = $(call github,morrownr,8812au-20210820,$(RTL8812AU_VERSION))
 RTL8812AU_LICENSE = GPL-2.0
 RTL8812AU_LICENSE_FILES = LICENSE
 
@@ -16,9 +16,6 @@ RTL8812AU_MODULE_MAKE_OPTS = \
 	USER_EXTRA_CFLAGS="-DCONFIG_$(call qstrip,$(BR2_ENDIAN))_ENDIAN \
 		-Wno-error"
 
-define RTL8812AU_MAKE_SUBDIR
-        (cd $(@D); ln -s . RTL8812AU)
-endef
 
 RTL8812AU_PRE_CONFIGURE_HOOKS += RTL8812AU_MAKE_SUBDIR
 
